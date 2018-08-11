d9a1c4f93ffc3dce2afc932d83c90de415a9df52
髙橋 佑太 <yuta.takahashi@jbs.com>
2018-08-11 22:47:06 +0900

    [fix] tsxに変更

--- App.tsx
+++ App.tsx
@@ -6,7 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react'
+import { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
@@ -21,7 +22,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>まじでふざけんなやっとか</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
